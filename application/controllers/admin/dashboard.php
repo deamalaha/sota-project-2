942a7ee0ffc16a0ccfb55529c924fe25ad85e164
moehzi <faizi1234098@gmail.com>
2020-12-12 16:40:39 +0800

    tambah dikit bagian itu tuh

--- application/controllers/admin/dashboard.php
+++ application/controllers/admin/dashboard.php
@@ -1,13 +1,15 @@
 <?php
-defined('BASEPATH') OR exit('No direct script access allowed');
+defined('BASEPATH') or exit('No direct script access allowed');
 
-class dashboard extends CI_Controller {
+class dashboard extends CI_Controller
+{
 
-    public function __construct() {
-        parent::__construct();
-        date_default_timezone_set('Asia/Makassar');
-        if($this->session->userdata('active') != "active"){//Belum Login
-             $alertDanger = '
+	public function __construct()
+	{
+		parent::__construct();
+		date_default_timezone_set('Asia/Makassar');
+		if ($this->session->userdata('active') != "active") { //Belum Login
+			$alertDanger = '
                 <div class="alert alert-danger alert-dismissible fade show" style="background-color:#c90831;color:white;">
                     <button type="button" class="close" data-dismiss="alert" aria-label="Close">
                         <span aria-hidden="true">&times;</span>
@@ -15,56 +17,60 @@ class dashboard extends CI_Controller {
                     <strong>Belum Login!</strong> Silahkan Login Terlebih dahulu.
                 </div>
             ';
-            $this->session->set_flashdata('info', $alertDanger);
-            redirect('admin/login');
-        }
-        $this->load->library('form_validation');
-    }
-    
-    public function index(){
-     $data = array(
-            'produk' => $this->Crud->ga('produk'),
-            'title' => 'Halaman Dashboad',
-            'isi' => 'admin/dashboard/isi',
-        );
-    $this->load->view('admin/_layouts/wrapper', $data);
-    }
+			$this->session->set_flashdata('info', $alertDanger);
+			redirect('admin/login');
+		}
+		$this->load->library('form_validation');
+	}
 
-    public function produk(){
-     $data = array(
-        'produk' => $this->Crud->ga('produk'),
-        'title' => 'Produk',
-        'isi' => 'admin/dashboard/data_produk',
-        );
-    $this->load->view('admin/_layouts/wrapper', $data);
-    }
+	public function index()
+	{
+		$data = array(
+			'produk' => $this->Crud->ga('produk'),
+			'title' => 'Halaman Dashboad',
+			'isi' => 'admin/dashboard/isi',
+		);
+		$this->load->view('admin/_layouts/wrapper', $data);
+	}
 
-    public function tambah_produk(){
-        $nama_produk = $this->input->post('nama_produk');
-        $keterangan  = $this->input->post('keterangan');
-        $kategori    = $this->input->post('kategori');
-        $harga       = $this->input->post('harga');
-        $stok        = $this->input->post('stok');
-        $gambar      = $_FILES['gambar']['name'];
-        if ($gambar = ''){}else{
-            $config ['upload_path'] = './assets/gambarProduk';
-            $config['allowed_types']    = 'jpg|png|jpeg';
+	public function produk()
+	{
+		$data = array(
+			'produk' => $this->Crud->ga('produk'),
+			'title' => 'Produk',
+			'isi' => 'admin/dashboard/data_produk',
+		);
+		$this->load->view('admin/_layouts/wrapper', $data);
+	}
 
-            $this->load->library('upload', $config);
-            if(!$this->upload->do_upload('gambar')){
-            }else{
-                $gambar=$this->upload->data('file_name');
-            }
-        }
-        $data = array(
-            'nama_produk'  => $nama_produk,
-            'keterangan'   => $keterangan,
-            'kategori'     => $kategori,
-            'harga'        => $harga,
-            'stok'         => $stok,
-            'gambar'       => $gambar
-        );
-        $info = '
+	public function tambah_produk()
+	{
+		$nama_produk = $this->input->post('nama_produk');
+		$keterangan  = $this->input->post('keterangan');
+		$kategori    = $this->input->post('kategori');
+		$harga       = $this->input->post('harga');
+		$stok        = $this->input->post('stok');
+		$gambar      = $_FILES['gambar']['name'];
+		if ($gambar = '') {
+		} else {
+			$config['upload_path'] = './assets/gambarProduk';
+			$config['allowed_types']    = 'jpg|png|jpeg';
+
+			$this->load->library('upload', $config);
+			if (!$this->upload->do_upload('gambar')) {
+			} else {
+				$gambar = $this->upload->data('file_name');
+			}
+		}
+		$data = array(
+			'nama_produk'  => $nama_produk,
+			'keterangan'   => $keterangan,
+			'kategori'     => $kategori,
+			'harga'        => $harga,
+			'stok'         => $stok,
+			'gambar'       => $gambar
+		);
+		$info = '
                      <div class="alert alert-success alert-dismissible fade show">
                         <button type="button" class="close" data-dismiss="alert" aria-label="Close">
                             <span aria-hidden="true">&times;</span>
@@ -72,23 +78,24 @@ class dashboard extends CI_Controller {
                         <strong>Berhasil !</strong> Data telah ditambah.
                     </div>
                 ';
-        $this->Crud->i('produk', $data);
-        $this->session->set_flashdata('info',$info);
-        redirect('admin/dashboard/produk/');
-    }
+		$this->Crud->i('produk', $data);
+		$this->session->set_flashdata('info', $info);
+		redirect('admin/dashboard/produk/');
+	}
 
-    public function edit_produk($id){
-        $where = array('id_produk' => $id);
-        $input = $this->input->post(NULL, TRUE);
-        $data = array(
-            'id_produk'     => $input['id_produk'],
-            'nama_produk'   => $input['nama_produk'],
-            'keterangan'    => $input['keterangan'],
-            'kategori'      => $input['kategori'],
-            'harga'         => $input['harga'],
-            'stok'          => $input['stok'],
-        );
-         $info = '
+	public function edit_produk($id)
+	{
+		$where = array('id_produk' => $id);
+		$input = $this->input->post(NULL, TRUE);
+		$data = array(
+			'id_produk'     => $input['id_produk'],
+			'nama_produk'   => $input['nama_produk'],
+			'keterangan'    => $input['keterangan'],
+			'kategori'      => $input['kategori'],
+			'harga'         => $input['harga'],
+			'stok'          => $input['stok'],
+		);
+		$info = '
                      <div class="alert alert-success alert-dismissible fade show">
                         <button type="button" class="close" data-dismiss="alert" aria-label="Close">
                             <span aria-hidden="true">&times;</span>
@@ -97,31 +104,32 @@ class dashboard extends CI_Controller {
                     </div>
                 ';
 
-        $this->Crud->u('produk', $data, $where);
-        $this->session->set_flashdata('info',$info);
-        redirect('admin/dashboard/produk');
-    }   
+		$this->Crud->u('produk', $data, $where);
+		$this->session->set_flashdata('info', $info);
+		redirect('admin/dashboard/produk');
+	}
 
-    public function edit(){
-        $id_produk  = $this->input->post('id');
-        $nama_produk  = $this->input->post('nama_produk');
-        $keterangan  = $this->input->post('keterangan');
-        $kategori  = $this->input->post('kategori');
-        $harga  = $this->input->post('harga');
-        $stok  = $this->input->post('stok');
+	public function edit()
+	{
+		$id  = $this->input->post('id');
+		$nama_produk  = $this->input->post('nama_produk');
+		$keterangan  = $this->input->post('keterangan');
+		$kategori  = $this->input->post('kategori');
+		$harga  = $this->input->post('harga');
+		$stok  = $this->input->post('stok');
 
-        $data = array(
-            'nama_produk'   => $nama_produk,
-            'keterangan'    => $keterangan,
-            'kategori'      => $kategori,
-            'harga'         => $harga,
-            'stok'          => $stok,
-        );
+		$data = array(
+			'nama_produk'   => $nama_produk,
+			'keterangan'    => $keterangan,
+			'kategori'      => $kategori,
+			'harga'         => $harga,
+			'stok'          => $stok,
+		);
 
-        $where = array(
-            'id_produk' => $id
-        );
-         $info = '
+		$where = array(
+			'id_produk' => $id
+		);
+		$info = '
                 <div class="alert alert-success alert-dismissible fade show">
                     <button type="button" class="close" data-dismiss="alert" aria-label="Close">
                         <span aria-hidden="true">&times;</span>
@@ -130,16 +138,17 @@ class dashboard extends CI_Controller {
                 </div>
                 ';
 
-        $this->Crud->u('produk', $data, $where);
-        $this->session->set_flashdata('info',$info);
-        redirect('admin/dashboard/produk');
-    }
+		$this->Crud->u('produk', $data, $where);
+		$this->session->set_flashdata('info', $info);
+		redirect('admin/dashboard/produk');
+	}
 
-    public function delete_produk($id){
-        $where = array(
-            'id_produk' => $id
-        );
-        $info = '
+	public function delete_produk($id)
+	{
+		$where = array(
+			'id_produk' => $id
+		);
+		$info = '
                 <div class="alert alert-danger alert-dismissible fade show" style="background-color:#c90831;color:white;">
                     <button type="button" class="close" data-dismiss="alert" aria-label="Close">
                         <span aria-hidden="true">&times;</span>
@@ -147,8 +156,8 @@ class dashboard extends CI_Controller {
                     <strong>Berhasil!</strong> Data telah dihapus.
                 </div>
             ';
-        $this->Crud->d('produk', $where);
-        $this->session->set_flashdata('info', $info);
-        redirect('admin/dashboard/produk');
-    }
+		$this->Crud->d('produk', $where);
+		$this->session->set_flashdata('info', $info);
+		redirect('admin/dashboard/produk');
+	}
 }
